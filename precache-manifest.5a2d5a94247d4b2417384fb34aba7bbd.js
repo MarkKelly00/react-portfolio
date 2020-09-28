196dbca691c02eb0fab7916d670e35619e6ead76
Mako3 <big.mako@gmail.com>
2020-09-27 22:21:58 -0700

    Updates

---
+++ precache-manifest.5a2d5a94247d4b2417384fb34aba7bbd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c0af631b27881a663664dc16b59e2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e028a49f7e0f01ad6a7b",
+    "url": "/static/css/main.e32907d8.chunk.css"
+  },
+  {
+    "revision": "d2f4e0d8374d84cff2d7",
+    "url": "/static/js/2.f29182af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f29182af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e028a49f7e0f01ad6a7b",
+    "url": "/static/js/main.c412f171.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "1b775e585ac7182cedf7e39e175ba47c",
+    "url": "/static/media/alertify_sample.1b775e58.png"
+  },
+  {
+    "revision": "a1336e6b01c70503f9abce2399261a42",
+    "url": "/static/media/birthdayAPI.a1336e6b.PNG"
+  },
+  {
+    "revision": "c6d702285b28bd8475cfad881fca18c9",
+    "url": "/static/media/burgerApp.c6d70228.png"
+  },
+  {
+    "revision": "2666aee7734211b1b859a3741f2529e8",
+    "url": "/static/media/note_keeper.2666aee7.png"
+  }
+]);
