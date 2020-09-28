c383d81d02ab6605185cdb78f43c78e9444503cf
Mako3 <big.mako@gmail.com>
2020-09-28 00:00:42 -0700

    Updates

---
+++ precache-manifest.b8c01c77dfcc194730effddbe13740b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c40481ac94e4ff7b35a1366aa47b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c07520e9549aaf5b77fd",
+    "url": "/static/css/main.565e536a.chunk.css"
+  },
+  {
+    "revision": "d2f4e0d8374d84cff2d7",
+    "url": "/static/js/2.f29182af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f29182af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07520e9549aaf5b77fd",
+    "url": "/static/js/main.fb20fee8.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "1b775e585ac7182cedf7e39e175ba47c",
+    "url": "/static/media/alertify_sample.1b775e58.png"
+  },
+  {
+    "revision": "a1336e6b01c70503f9abce2399261a42",
+    "url": "/static/media/birthdayAPI.a1336e6b.PNG"
+  },
+  {
+    "revision": "c6d702285b28bd8475cfad881fca18c9",
+    "url": "/static/media/burgerApp.c6d70228.png"
+  },
+  {
+    "revision": "2666aee7734211b1b859a3741f2529e8",
+    "url": "/static/media/note_keeper.2666aee7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.64319e9fac6220be70229ed43e566b25.js"
+  "/precache-manifest.b8c01c77dfcc194730effddbe13740b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
